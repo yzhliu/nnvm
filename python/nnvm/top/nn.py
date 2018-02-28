415d75ac8c95f44edcd29605877f22ee0e48f995
Yizhi Liu <yizhiliu@amazon.com>
2018-02-28 14:45:29 -0800

    pass kernel_size to conv2d_prepack compute & schedule

--- python/nnvm/top/nn.py
+++ python/nnvm/top/nn.py
@@ -96,18 +96,19 @@ def schedule_conv2d(attrs, outs, target):
 reg.register_pattern("conv2d", OpPattern.OUT_ELEMWISE_FUSABLE)
 
 @reg.register_compute("conv2d_prepack")
-def compute_conv2d(attrs, inputs, _):
+def compute_conv2d_prepack(attrs, inputs, _):
     """Compute definition of conv2d"""
     padding = attrs.get_int_tuple("padding")
     strides = attrs.get_int_tuple("strides")
     dilation = attrs.get_int_tuple("dilation")
+    kernel_size = attrs.get_int_tuple('kernel_size')
     groups = attrs.get_int("groups")
     channels = attrs.get_int("channels")
     layout = attrs["layout"]
     assert layout == "NCHW", "only support nchw for now"
     assert dilation == (1, 1), "not support dilate now"
     if groups == 1:
-        out = topi.nn.conv2d_prepack(inputs[0], inputs[1], strides, padding)
+        out = topi.nn.conv2d_prepack(inputs[0], inputs[1], kernel_size, strides, padding)
     else:
         raise ValueError("not support arbitrary group number > 1 for now")
     if attrs.get_bool("use_bias"):
@@ -117,12 +118,13 @@ def compute_conv2d(attrs, inputs, _):
     return out
 
 @reg.register_schedule("conv2d_prepack")
-def schedule_conv2d(attrs, outs, target):
+def schedule_conv2d_prepack(attrs, outs, target):
     """Schedule definition of conv2d"""
     groups = attrs.get_int("groups")
+    kernel_size = attrs.get_int_tuple('kernel_size')
     with tvm.target.create(target):
         if groups == 1:
-            return topi.generic.schedule_conv2d_prepack(outs)
+            return topi.generic.schedule_conv2d_prepack(kernel_size, outs)
         else:
             raise ValueError("not support group number > 1 for now")
 
